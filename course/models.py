1617d73abea7bb2d8c5f7f6a470644cd92d82b23
Nishita Doraiswamy <YOUR_GITHUB_EMAIL>
2026-09-12 21:23:24 +0530

    Add Instructor and Learner models

--- course/models.py
+++ course/models.py
@@ -1,6 +1,46 @@
 from django.db import models
 
 
+class Instructor(models.Model):
+    user = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE
+    )
+    full_time = models.BooleanField(default=True)
+    total_learners = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Learner(models.Model):
+    user = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE
+    )
+    STUDENT = "student"
+    DEVELOPER = "developer"
+    DATA_SCIENTIST = "data_scientist"
+    DATABASE_ADMIN = "dba"
+
+    OCCUPATION_CHOICES = [
+        (STUDENT, "Student"),
+        (DEVELOPER, "Developer"),
+        (DATA_SCIENTIST, "Data Scientist"),
+        (DATABASE_ADMIN, "Database Admin"),
+    ]
+
+    occupation = models.CharField(
+        max_length=20,
+        choices=OCCUPATION_CHOICES,
+        default=STUDENT
+    )
+    social_link = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.user.username + "," + self.occupation
+
+
 class Course(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
